cbf8871c156362f2b92e9a622712c959fe05a112
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-10 08:04:29 +0000

    Worked on pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened.
-    
-    Args:
-        boxes (list of lists): List of boxes with keys.
-    
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
+
     opened = set()
     stack = [0]
-
     while stack:
         box = stack.pop()
         if box not in opened:
@@ -33,4 +24,3 @@ if __name__ == "__main__":
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
     print(canUnlockAll(boxes))  # False
-
