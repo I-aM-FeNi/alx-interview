0c2cb544d2e24e45e88a3e801ae42dff4524b13f
I-aM-FeN <fenimabusa@gmail.com>
2024-10-11 04:31:29 +0200

    0x01. Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A method that determines if all the boxes can be opened.
+
+Prototype: def canUnlockAll(boxes)
+boxes is a list of lists
+A key with the same number as a box opens that box
+You can assume all keys will be positive integers
+There can be keys that do not have boxes
+The first box boxes[0] is unlocked
+Return True if all boxes can be opened, else return False
+"""
+
+
+def canUnlockAll(boxes):
+    """Return True if all boxes can be opened, else return False"""
+    n = len(boxes)
+    unlocked = set([0])  # Start with box 0 unlocked
+    keys = set(boxes[0])  # Keys from the first box
+
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)
+            keys.update(boxes[new_key])
+
+    return len(unlocked) == n
