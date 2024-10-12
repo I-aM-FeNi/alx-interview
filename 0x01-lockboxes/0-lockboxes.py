dfcc3c2d194842336ebcb1c2b3956987028b557a
malcolms-anatomy <malcolmihere@outlook.com>
2024-10-12 03:20:52 +0000

    Updated filese

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,11 +2,16 @@
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
+
     Args:
         boxes (list of lists): List of boxes with keys.
+
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
+    if not boxes:
+        return False
+
     opened = set()
     stack = [0]
 
@@ -15,7 +20,7 @@ def canUnlockAll(boxes):
         if box not in opened:
             opened.add(box)
             for key in boxes[box]:
-                if key not in opened:
+                if key < len(boxes) and key not in opened:
                     stack.append(key)
 
     return len(opened) == len(boxes)
@@ -32,3 +37,22 @@ if __name__ == "__main__":
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
     print(canUnlockAll(boxes))  # False
 
+    boxes = [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))  # False
+
+    boxes = [[0]]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [
+        [10, 3, 8, 9, 6, 5, 8, 1], [8, 5, 3, 7, 1, 8, 6], [5, 1, 9, 1], [],
+        [6, 6, 9, 4, 3, 2, 3, 8, 5], [9, 4], [4, 2, 5, 1, 1, 6, 4, 5, 6],
+        [9, 5, 8, 8], [6, 2, 8, 6]
+    ]
+    print(canUnlockAll(boxes))  # True
+
+    boxes = [
+        [7, 5], [1, 10, 7], [9, 6, 10], [7, 9], [2], [7, 3],
+        [7, 9, 10, 10, 8, 9, 2, 5], [7, 2, 2, 4, 4, 2, 4, 8, 7],
+        [4, 2, 9, 6, 6, 5, 5]
+    ]
+    print(canUnlockAll(boxes))  # False
